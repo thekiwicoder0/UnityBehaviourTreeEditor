57927c00593d5e07fafa6518df1e3dc37e092af7
Mash <thekiwicoder@gmail.com>
2022-10-31 21:52:52 +1300

    Updating readme file. Removing old scripts..

--- build_package.bat
+++
@@ -1,3 +0,0 @@
-"D:\Unity\2022.1.20f1\Editor\Unity.exe" -batchmode -nographics -quit -projectPath "D:\TheKiwiCoder\UnityBehaviourTreeEditor" -exportPackage "Assets" "behaviour-tree-editor.unitypackage"
-
-pause
--- build_zip.bat
+++
@@ -1,3 +0,0 @@
-tar.exe -a -c -f behaviour-tree-editor.zip behaviour-tree-editor.unitypackage README.pdf
-
-pause
