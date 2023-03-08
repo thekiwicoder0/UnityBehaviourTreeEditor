8aef0a6dd897b556cdf65729c9f5741b72068c84
Mash <thekiwicoder@gmail.com>
2023-03-08 17:52:18 +1300

    Added support for renaming blackboard keys

--- Assets/BehaviourTree/CHANGELOG.md
+++ Assets/BehaviourTree/CHANGELOG.md
@@ -7,6 +7,12 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/)
 Note this package is still in very early development and in high flux. 
 Version numbers don't follow semantic versioning yet, and data format may change in future which will be highlighted in this document.
 
+# [0.0.18] - 08-03-2023
+
+## Rename Blackboard Keys
+
+Blackboard Keys can now be renamed. Note: any selected nodes referencing the blackboard keys won't update until the node is next selected.
+
 # [0.0.17] - 04-03-2023
 
 ## NodeProperty<T>s, Blackboard Overrides, Switch Node
--- Assets/BehaviourTree/Editor/PropertyDrawers/BlackboardKeyPropertyDrawer.cs
+++ Assets/BehaviourTree/Editor/PropertyDrawers/BlackboardKeyPropertyDrawer.cs
@@ -12,16 +12,48 @@ namespace TheKiwiCoder {
     
         public override VisualElement CreatePropertyGUI(SerializedProperty property) {
             if (property.propertyType != SerializedPropertyType.ArraySize) {
-                PropertyField field = new PropertyField();
-                
-                field.label = $"{property.displayName}";
-                field.bindingPath = nameof(BlackboardKey<object>.value);
 
-                field.AddManipulator(new ContextualMenuManipulator((evt) => {
+                Label keyName = new Label();
+                TextField renameField = new TextField();
+                PropertyField keyValue = new PropertyField();
+                VisualElement container = new VisualElement();
+
+                keyName.bindingPath = nameof(BlackboardKey.name);
+                keyName.AddToClassList("unity-base-field__label");
+
+                renameField.style.display = DisplayStyle.None;
+                renameField.bindingPath = nameof(BlackboardKey.name);
+                renameField.RegisterCallback<BlurEvent>((evt) => {
+                    keyValue.style.display = DisplayStyle.Flex;
+                    keyName.style.display = DisplayStyle.Flex;
+                    renameField.style.display = DisplayStyle.None;
+                });
+
+                keyValue.label = "";
+                keyValue.style.flexGrow = 1.0f;
+                keyValue.bindingPath = nameof(BlackboardKey<object>.value);
+                keyValue.AddManipulator(new ContextualMenuManipulator((evt) => {
                     evt.menu.AppendAction("Delete", (x) => BehaviourTreeEditorWindow.Instance.serializer.DeleteBlackboardKey(property.displayName), DropdownMenuAction.AlwaysEnabled);
                 }));
-                    
-                return field;
+
+                container.style.flexDirection = FlexDirection.Row;
+                container.Add(keyName);
+                container.Add(renameField);
+                container.Add(keyValue);
+
+                keyName.RegisterCallback<MouseDownEvent>((evt) => {
+                    if(evt.clickCount == 2) {
+                        renameField.value = keyName.text;
+                        renameField.style.display = DisplayStyle.Flex;
+                        renameField.Focus();
+
+                        keyValue.style.display = DisplayStyle.None;
+                        keyName.style.display = DisplayStyle.None;
+                    }
+                });
+
+
+                return container;
             }
             return null;
         }
