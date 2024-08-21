3363cb204b3e027a2856d772a6972a14d848a5d5
Grzes Furga <grzesiof@gmail.com>
2024-08-21 17:10:38 +0200

    Dont restart nodes that already returned success

--- Runtime/Composites/Sequencer.cs
+++ Runtime/Composites/Sequencer.cs
@@ -5,8 +5,10 @@ using UnityEngine;
 namespace TheKiwiCoder {
     [System.Serializable]
     public class Sequencer : CompositeNode {
+        int lastNodeFinished;
 
         protected override void OnStart() {
+            lastNodeFinished = -1;
         }
 
         protected override void OnStop() {
@@ -14,12 +16,16 @@ namespace TheKiwiCoder {
 
         protected override State OnUpdate() {
             for (int i = 0; i < children.Count; ++i) {
+                if (i <= lastNodeFinished) continue;
+
                 var childStatus = children[i].Update();
-                
+
                 if (childStatus == State.Running) {
                     return State.Running;
                 } else if (childStatus == State.Failure) {
                     return State.Failure;
+                } else if (childStatus == State.Success) {
+                    lastNodeFinished = i;
                 }
             }
 
