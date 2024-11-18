cf791fef610de8460f58edd21627bcb1dfd1de0d
Maximilien GALEA <maximilien.galea@gmail.com>
2024-11-18 13:32:54 +0100

    change value to 1.0f for clarity

--- Runtime/Actions/Wait.cs
+++ Runtime/Actions/Wait.cs
@@ -7,7 +7,7 @@ namespace TheKiwiCoder {
     [System.Serializable]
     public class Wait : ActionNode {
 
-		[Tooltip("Amount of time to wait before returning success")] public NodeProperty<float> duration = new NodeProperty<float>() { Value = 1 };
+		[Tooltip("Amount of time to wait before returning success")] public NodeProperty<float> duration = new NodeProperty<float>() { Value = 1.0f };
         
         float startTime;
 
